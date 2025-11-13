6f685b651bdccb860aa67fcf4fe82d39fa3301bb
Filip Hajdyła <hajdylaf@gmail.com>
2025-11-13 20:02:33 +0100

    add config script

---
+++ configure.sh
@@ -0,0 +1,59 @@
+echo "==================================="
+echo "ARCH LINUX POST-INSTALLATION SCRIPT"
+echo "==================================="
+echo
+echo "Developed by: hajdylaf"
+echo
+read -p 'Enter e-mail: ' USER_EMAIL
+
+# update system
+sudo pacman -Syuu --noconfirm
+
+# install system packages
+sudo pacman -Sy --needed --noconfirm \
+    bat \
+    discord \
+    dust \
+    fzf \
+    gnome-browser-connector \
+    go \
+    less \
+    libreoffice-fresh \
+    man \
+    ncdu \
+    neovim \
+    rsync \
+    signal-desktop \
+    tldr \
+    tmux \
+    tree \
+    xclip
+
+# clear pacman cache
+sudo pacman -Scc --noconfirm
+
+# install yay and clean up
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -i
+cd -
+rm -rf yay
+
+# install yay packages
+yay -Sy --needed \
+    gnome-shell-extension-desktop-icons-ng \
+    vscodium
+
+# clear yay cache
+yay -Scc
+
+# sync dotfiles
+git clone https://github.com/hajdylaf/dotfiles.git
+cd dotfiles
+rsync -r home/.* $HOME/.
+rsync -r home/* $HOME/.
+cd -
+rm -rf dotfiles
+
+# create ssh key
+ssh-keygen -t ecdsa -C $USER_EMAIL
